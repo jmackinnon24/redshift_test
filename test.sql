894b8f927f32cdcd3a2e0d71bd2474351c3c2afd
jmackinnon24 <95762562+jmackinnon24@users.noreply.github.com>
2022-06-12 23:07:21 -0400

    Create test.sql

---
+++ test.sql
@@ -0,0 +1,17 @@
+CREATE TABLE zz_jmackinnon.test2 (
+	outreach_need_smart_key varchar(120) NULL,
+	campaign_slug varchar(120) NULL,
+	target_id varchar(40) NULL,
+	target_type_id int4 NULL,
+	subject_id varchar(40) NULL,
+	subject_type_id int4 NULL,
+	priority int4 NULL,
+	archived timestamptz NULL,
+	created_on date NULL,
+	created_by varchar(256) NULL,
+	updated_on date NULL,
+	updated_by varchar(256) NULL,
+	subject_group_id varchar(256) NULL,
+	target_group_id varchar(256) NULL,
+	product_type_id int4 NULL
+);
